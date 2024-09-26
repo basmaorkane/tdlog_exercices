02b68f6bb0a14819f544a1402416dfba0c32ac32
basmaorkane <basma.orkane@centrale-casablanca.ma>
2024-09-26 11:55:35 +0100

    Update exo2.py

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -28,3 +28,39 @@ fixed_tests_False = (
     ( "spam",    "eggs"  )
 )
 """
+import unittest
+def solution(string, ending):
+    return string.endswith(ending)
+
+
+class TestSolution(unittest.TestCase):
+    
+    def test_fixed_tests_true(self):
+        fixed_tests_True = (
+            ("samurai", "ai"),
+            ("ninja",   "ja"),
+            ("sensei",  "i"),
+            ("abc",     "abc"),
+            ("abcabc",  "bc"),
+            ("fails",   "ails"),
+        )
+        for s, e in fixed_tests_True:
+            with self.subTest(s=s, e=e):
+                self.assertTrue(solution(s, e))
+    
+    def test_fixed_tests_false(self):
+        fixed_tests_False = (
+            ("sumo",    "omo"),
+            ("samurai", "ra"),
+            ("abc",     "abcd"),
+            ("ails",    "fails"),
+            ("this",    "fails"),
+            ("spam",    "eggs"),
+        )
+        for s, e in fixed_tests_False:
+            with self.subTest(s=s, e=e):
+                self.assertFalse(solution(s, e))
+
+# Pour exécuter les tests
+if __name__ == "__main__":
+    unittest.main()
